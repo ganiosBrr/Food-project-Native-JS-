483fa062f5812bd50e1b977d32af01bf35818166
Ganios <nurpeisovganibek@gmail.com>
2022-11-01 23:06:54 +0600

    using classes and rest operator fix menu items

--- js/script.js
+++ js/script.js
@@ -137,7 +137,7 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     });
 
-    const modalTimerId = setTimeout(openModal, 3000);
+    //const modalTimerId = setTimeout(openModal, 3000);
 
     function modalScroll() {
       if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -152,12 +152,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
   //Используем классы для блоков меню
   class MenuCard {
-    constructor(src, alt, title, desc, price, transfer) {
+    constructor(src, alt, title, desc, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.desc = desc;
       this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
       this.transfer = 462;
       this.convertCurrency();
     }
@@ -168,8 +170,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
     render() {
       const element = document.createElement('div');
+      
+      if (this.classes.length === 0) {
+          this.element = 'menu__item';
+          element.classList.add(this.element);
+      } else {
+          this.classes.forEach(className => {element.classList.add(className)});
+      }
+
       element.innerHTML = `
-      <div class="menu__item">
         <img src="${this.src}" alt="${this.alt}">
         <h3 class="menu__item-subtitle">${this.title}</h3>
         <div class="menu__item-descr">${this.desc}</div>
@@ -178,16 +187,36 @@ window.addEventListener('DOMContentLoaded', () => {
             <div class="menu__item-cost">Цена:</div>
             <div class="menu__item-total"><span>${this.price}</span> тг/день</div>
         </div>
-      </div>`;
-      const container = document.querySelector('.menu .container');
-      container.append(element);
+      `;
+      this.parent.append(element);
     }
   }
 
-  const div = new MenuCard(
+  new MenuCard (
     'img/tabs/vegy.jpg',
     'vegy',
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    '1'
+    '1',
+    '.menu .container',
+    'menu__item',
+    'bag'
+  ).render();
+
+  new MenuCard (
+    'img/tabs/elite.jpg',
+    'elite',
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    '2',
+    '.menu .container'
+  ).render();
+
+  new MenuCard (
+    'img/tabs/post.jpg',
+    'post',
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    '3',
+    '.menu .container'
   ).render();
